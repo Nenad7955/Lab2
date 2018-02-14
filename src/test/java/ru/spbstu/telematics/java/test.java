e84e942bbd88e6495ddaf4fdff7f0c6466897ca5
Nenad7955 <32124532+Nenad7955@users.noreply.github.com>
2018-02-14 23:16:01 +0300

    Update test.java

--- src/test/java/ru/spbstu/telematics/java/test.java
+++ src/test/java/ru/spbstu/telematics/java/test.java
@@ -20,32 +20,58 @@ public class test{
     public test(){
         totest = new HashSet<>();
         normHash = new java.util.HashSet<Integer>();
+
+
         for(int i=0;i<=15;i++){
-            totest.add(i);
-            normHash.add(i);
+            totest.add(i);  normHash.add(i);
         }
     }
 
-    @Test
-    public void test(){
+    @Test //has next
+    public void test1(){
         Iterator<Integer> testiter =  totest.iterator();
         Iterator<Integer> javaiter =  normHash.iterator();
-
-
-        assertEquals("IteratorHasNext Fail!",javaiter.hasNext(),testiter.hasNext());
-        assertEquals("IteratorNext Fail!",javaiter.next(),testiter.next());
-
-        //go to next element
-        javaiter.next();
-        testiter.next();
-
-        //delete it
-        javaiter.remove();
-        testiter.remove();
-
-        assertEquals("IteratorRemove Fail!",javaiter.next(), testiter.next());
-        assertEquals("IsEmpty Fail!",normHash.isEmpty(), totest.isEmpty());
-        assertEquals("Contains Fail!",normHash.contains(3), totest.contains(3));
-
+        assertEquals("Something went wrong",javaiter.hasNext(),testiter.hasNext());
     }
+    @Test //next
+    public void test2() {
+        Iterator<Integer> testiter =  totest.iterator();
+        Iterator<Integer> javaiter =  normHash.iterator();
+        assertEquals("IteratorNext Fail!", javaiter.next(), testiter.next());
+    }
+
+    @Test //next
+    public void test3() {
+        Iterator<Integer> testiter =  totest.iterator();
+        Iterator<Integer> javaiter =  normHash.iterator();
+        //go to next element
+        javaiter.next();  testiter.next();
+        //delete it
+        javaiter.remove();  testiter.remove();
+        assertEquals("IteratorRemove Fail!", javaiter.next(), testiter.next());
+    }
+
+    @Test //next -> isempty
+    public void test4() {
+        Iterator<Integer> testiter =  totest.iterator();
+        Iterator<Integer> javaiter =  normHash.iterator();
+        //go to next element
+        javaiter.next();  testiter.next();
+        //delete it
+        javaiter.remove();  testiter.remove();
+        assertEquals("IsEmpty Fail!",normHash.isEmpty(), totest.isEmpty());
+    }
+
+    @Test //next ->contains number 5
+    public void test5() {
+        Iterator<Integer> testiter =  totest.iterator();
+        Iterator<Integer> javaiter =  normHash.iterator();
+        //go to next element
+        javaiter.next();  testiter.next();
+        //delete it
+        javaiter.remove();  testiter.remove();
+        assertEquals("Contains Fail!",normHash.contains(5), totest.contains(5));
+    }
+
+
 }
